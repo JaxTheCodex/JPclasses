41dd08cdfe6f9372671026345b21e28d8ced79b3
JaxTheCodex <jagarnathpatro836@gmail.com>
2025-06-11 08:12:39 +0530

    fixed the path issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
-import logo from '../assets/logo.png';
+import logo from '../assets/Logo.png';
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
